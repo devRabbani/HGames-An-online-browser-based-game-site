2176c75576849c0fd6ac76bd727a8c83db4ddbc1
StrikerOne65 <golamrabbani20014@gmail.com>
2020-08-22 20:41:42 +0530

    fw

--- index.js
+++
@@ -1,1 +0,0 @@
-navigator.serviceWorker && navigator.serviceWorker.register(‘./sw.js’).then(function(registration) {  console.log(‘Excellent, registered with scope: ‘, registration.scope);});
--- sw.js
+++ sw.js
@@ -1,9 +1,33 @@
-/** An empty service worker! */
-
-self.addEventListener (‘fetch’, function(event)
-
-{
-
-/** An empty fetch handler! */
-
+/const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
