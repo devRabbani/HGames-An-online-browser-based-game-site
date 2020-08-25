1b4926d6b78d2f2bcea3d3102a7bd177a926becd
StrikerOne65 <golamrabbani20014@gmail.com>
2020-08-25 19:43:27 +0530

    fixing html

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log('linked');
 var main=document.querySelector('.main');
+window.addEventListener('load', async () => {
+console.log('linked');
+await apdate();
+});
+
+
 async function apdate(){
     const res = await fetch('https://h5games.online/freegames.json');
     const json= await res.json();
@@ -8,6 +13,4 @@ async function apdate(){
         <a href=/play.html?s=${game.link}><img src=${game.thumb}></a>
         </div>`
     }).join('\n')
-}
-apdate()
-
+};
